6f805ab961a55c469473128e2c73059a16558b98
chloe <csabijon@s6c.mlp.college>
2023-01-27 20:47:28 +0000

    i'm sorry i made it dadatabase :')

--- ukrcca/settings.py
+++ ukrcca/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local
+    'dadatabase.apps.DadatabaseConfig',
 ]
 
 MIDDLEWARE = [
