3cc6a4c44066a67bcf239f00e347e83ccdc61dcc
Finn G <fgallagher@s6c.mlp.college>
2023-01-31 09:43:24 +0000

    initialised django + tested some webpages

---
+++ files/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def test(request):
+    return render(request, "test.html")
+
+def logIn(request):
+    return render(request, "logIn.html")
+
+def base(request):
+    return render(request, "base.html")
