ac5ee997e9a85c64eccabb612f68639e81f16cea
Kat Dymond <kdymond@s6c.mlp.college>
2023-02-10 13:32:51 +0000

    everything

--- files/views.py
+++ files/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def test(request):
     return render(request, 'test.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def lboard(request):
+    return render(request, 'leaderboard.html')
+
+def score(request):
+    return render(request, 'score.html')
+
+def submit(request):
+    return render(request, 'submit.html')
