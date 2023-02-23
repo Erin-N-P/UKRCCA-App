fb32303ca8151540574922796696a54d53e37b19
Erin Penn <epenn@s6c.mlp.college>
2023-02-23 20:21:29 +0000

    Its not working

--- UKRCCA/admin_app/files/urls.py
+++ UKRCCA/admin_app/files/urls.py
@@ -1,6 +1,17 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from files import views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import redirect_view
 
 urlpatterns = [
-    path('files/', views.files, name='files'),
-]
+    path('admin/', admin.site.urls),
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('competitions/', views.comps, name='comps'),
+    path('leaderboard/', views.leadb, name='leadb'),
+    path('create competition/', views.compcre, name='compcre'),
+    path('base/', views.base, name='base'),
+    path('/home', redirect_view)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- UKRCCA/admin_app/files/views.py
+++ UKRCCA/admin_app/files/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import redirect
 
 def files(request):
-    template = loader.get_template('comps.html')
-    return HttpResponse(template.render())
+    return render(request, 'base.html')
+
+def files(request):
+    return render(request, 'compcre.html')
+
+def files(request):
+    return render(request, 'comps.html')
+
+def files(request):
+    return render(request, 'home.html')
+
+def files(request):
+    return render(request, 'leadb.html')
+
+def files(request):
+    return render(request, 'login.html')
+
+def redirect_view(request):
+    response = redirect('/home')
+    return response
