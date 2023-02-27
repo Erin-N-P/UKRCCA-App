a2820dff4c93e3d07664cdfaa31131f04e2ad338
chloe <csabijon@s6c.mlp.college>
2023-02-27 17:09:46 +0000

    dk what i did

--- database/forms.py
+++ database/forms.py
@@ -6,6 +6,28 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = NewUser
         fields = ('user_name','first_name', 'last_name', 'email')
+        widgets = {
+            'user_name': forms.TextInput(attrs={
+            'placeholder':'User Name',
+            'class':'form-control',
+            'style':'padding-left: 10px; width: 310px'
+            }),
+            'first_name': forms.TextInput(attrs={
+            'placeholder':'First Name',
+            'class':'form-control',
+            'style':'padding-left: 10px; width: 310px'
+            }),
+            'last_name': forms.TextInput(attrs={
+            'placeholder':'Last Name',
+            'class':'form-control',
+            'style':'padding-left: 10px; width: 310px'
+            }),
+            'email': forms.EmailInput(attrs={
+            'placeholder':'Email',
+            'class':'form-control',
+            'style':'padding-left: 10px; width: 310px'
+            })
+        }
 
     def __init__(self, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
